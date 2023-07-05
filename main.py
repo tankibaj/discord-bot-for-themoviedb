ac2d84ad9f7f7a62ebe1ade1d64867b7b8733858
Naim Ahmmed <mail@thenaim.com>
2023-07-05 22:02:39 +0200

    init: hello git

---
+++ main.py
@@ -0,0 +1,122 @@
+import os
+import discord
+from discord.ext import commands
+from themoviedb import TheMovieDB
+from dotenv import find_dotenv, load_dotenv
+import logging
+
+load_dotenv(find_dotenv())
+
+# Set up logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+DISCORD_CLIENT_SECRET = os.environ.get("DISCORD_CLIENT_SECRET")
+MESSAGE_RETENTION_TIME = os.environ.get("MESSAGE_RETENTION_TIME", 60)
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+
+def format_movie_data(movie_data):
+    title = movie_data.get('title')
+    original_title = movie_data.get('original_title')
+    name = movie_data.get('name')
+    release_date = movie_data.get('release_date')
+    # overview = movie_data.get('overview')
+    rating = movie_data.get('vote_average')
+    media_type = movie_data.get('media_type')
+    # poster = f"https://image.tmdb.org/t/p/w1280{movie_data.get('poster_path')}"
+    id = movie_data.get('id')
+    url = f"https://www.themoviedb.org/{media_type}/{id}"
+
+    return f":rocket: **Title**: {title} | {original_title} | {name}\n:cricket_game: **Rating**: {rating}\n:calendar: **Release Date**: {release_date}\n:film_frames: **Media Type**: {media_type}\n:link: **URL**: {url}\n-----\n\n\n\n\n\n"
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send('Invalid command used.')
+    else:
+        logger.error(f"An error occurred: {str(error)}")
+
+
+@bot.command(name='hindi_movie')
+async def hindi_movie(ctx):
+    try:
+        movie_db = TheMovieDB()
+        movies = movie_db.get_this_weeks_movies('hindi')
+        for movie in movies.get('results', []):
+            content = format_movie_data(movie)
+            msg = await ctx.send(content)
+            await msg.delete(delay=MESSAGE_RETENTION_TIME)
+    except Exception as e:
+        logger.error(f"An error occurred: {str(e)}")
+
+
+@bot.command(name='hindi_shows')
+async def hindi_shows(ctx):
+    try:
+        movie_db = TheMovieDB()
+        shows = movie_db.get_this_weeks_shows('hindi')
+        for show in shows.get('results', []):
+            content = format_movie_data(show)
+            msg = await ctx.send(content)
+            await msg.delete(delay=MESSAGE_RETENTION_TIME)
+    except Exception as e:
+        logger.error(f"An error occurred: {str(e)}")
+
+
+@bot.command(name='trending')
+async def trending(ctx):
+    try:
+        movie_db = TheMovieDB()
+        trending = movie_db.get_trending('week')
+        for trend in trending.get('results', []):
+            content = format_movie_data(trend)
+            msg = await ctx.send(content)
+            await msg.delete(delay=MESSAGE_RETENTION_TIME)
+    except Exception as e:
+        logger.error(f"An error occurred: {str(e)}")
+
+
+@bot.command(name='trending_today')
+async def trending_today(ctx):
+    try:
+        movie_db = TheMovieDB()
+        trending = movie_db.get_trending('day')
+        for trend in trending.get('results', []):
+            content = format_movie_data(trend)
+            msg = await ctx.send(content)
+            await msg.delete(delay=MESSAGE_RETENTION_TIME)
+    except Exception as e:
+        logger.error(f"An error occurred: {str(e)}")
+
+
+@bot.command(name='upcoming')
+async def upcoming(ctx):
+    try:
+        movie_db = TheMovieDB()
+        upcoming = movie_db.get_upcoming('movie')
+        for movie in upcoming.get('results', []):
+            content = format_movie_data(movie)
+            msg = await ctx.send(content)
+            await msg.delete(delay=MESSAGE_RETENTION_TIME)
+    except Exception as e:
+        logger.error(f"An error occurred: {str(e)}")
+
+
+@bot.command(name='shutdown')
+@commands.is_owner()  # This ensures only the owner of the bot can use this command
+async def shutdown(ctx):
+    await ctx.send('Shutting down...')
+    await ctx.bot.close()
+
+
+def main():
+    bot.run(DISCORD_CLIENT_SECRET)
+
+
+if __name__ == '__main__':
+    main()
---
+++ themoviedb/themoviedb.py
@@ -0,0 +1,74 @@
+import os
+import requests
+from datetime import datetime, timedelta
+import logging
+from dotenv import find_dotenv, load_dotenv
+
+
+class TheMovieDB:
+    def __init__(self):
+        load_dotenv(find_dotenv())
+        self.logger = logging.getLogger(__name__)
+        self.themoviedb_api_key = os.environ.get("THEMOVIEDB_API_KEY")
+        self.themoviedb_api_url = 'https://api.themoviedb.org/3'
+        self.discover_movie_url = f"{self.themoviedb_api_url}/discover/movie"
+        self.discover_show_url = f"{self.themoviedb_api_url}/discover/tv"
+        self.headers = {
+            "accept": "application/json",
+            "Authorization": f"Bearer {self.themoviedb_api_key}"
+        }
+        self.date_7_days_ago = (datetime.now() - timedelta(days=7)).strftime('%Y-%m-%d')
+        self.todays_date = datetime.now().strftime('%Y-%m-%d')
+        self.params = self.get_params('en-US', 'US', 'en')
+        self.params_hindi = self.get_params('en-US', 'IN', 'hi')
+
+    def get_common_params(self):
+        return {
+            'language': 'en-US',
+            'region': 'US',
+            'page': '1',
+            "video": 'true',
+            'adult': 'true',
+        }
+
+    def get_params(self, language, region, original_language):
+        return {
+            'language': language,
+            'region': region,
+            'with_original_language': original_language,
+            'sort_by': 'release_date.desc',
+            'primary_release_date.gte': self.date_7_days_ago,
+            'primary_release_date.lte': self.todays_date,
+            'page': '1',
+            'include_adult': 'false',
+            'include_video': 'false',
+        }
+
+    def get_themoviedb(self, url, params):
+        try:
+            response = requests.get(url, params=params, headers=self.headers)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            self.logger.error(f"Error getting data from theMovieDB: {e}")
+            return None
+
+    def get_this_weeks_movies(self, language):
+        params = self.params_hindi if language == 'hindi' else self.params
+        return self.get_themoviedb(self.discover_movie_url, params)
+
+    def get_this_weeks_shows(self, language):
+        params = self.params_hindi if language == 'hindi' else self.params
+        return self.get_themoviedb(self.discover_show_url, params)
+
+    def get_now_playing(self, media_type):
+        url = f"{self.themoviedb_api_url}/{media_type}/now_playing"
+        return self.get_themoviedb(url, self.get_common_params())
+
+    def get_trending(self, time_window):
+        url = f"{self.themoviedb_api_url}/trending/all/{time_window}"
+        return self.get_themoviedb(url, self.get_common_params())
+
+    def get_upcoming(self, media_type):
+        url = f"{self.themoviedb_api_url}/{media_type}/upcoming"
+        return self.get_themoviedb(url, self.get_common_params())
